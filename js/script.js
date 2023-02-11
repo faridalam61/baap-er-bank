fe1bfaf153e81ca398ee7335fc41aac1794c4706
Farid Alam <faridalam62@gmail.com>
2023-02-11 23:37:40 +0600

    js updated

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ const userEmail = document.getElementById('user-email');
 const userPass = document.getElementById('user-pass');
 const loginBtn = document.getElementById('login-btn')
 
+userPass.addEventListener('keyup', function () {
+    if (userEmail.value != '' && userPass.value != '') {
+    loginBtn.removeAttribute('disabled')
+    console.log('ok')
+}
+})
+
+userEmail.addEventListener('keyup', function () {
+    if (userEmail.value != '' && userPass.value != '') {
+    loginBtn.removeAttribute('disabled')
+    console.log('ok')
+}
+})
+
 loginBtn.addEventListener('click', function () {
   
     if (userEmail.value == 'faridalam62@gmail.com' && userPass.value == '123456789') {
